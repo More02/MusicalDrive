50abca5161b3c2d81ec01153230402dfcd1e7fed
Marina <mbakanova2016@yandex.ru>
2023-05-19 19:49:07 +0300

    Moved to server

--- app/src/main/java/com/example/commercialdirector/myitschool/connection/AppConfig.java
+++ app/src/main/java/com/example/commercialdirector/myitschool/connection/AppConfig.java
@@ -4,12 +4,12 @@ package com.example.commercialdirector.myitschool.connection;
 public class AppConfig {
 
 
-    public static String URL_LOGIN = "http://192.168.1.2/login.php";
+    public static String URL_LOGIN = "http://mbakanova2.temp.swtest.ru/login.php";
 
 
-    public static String URL_REGISTER = "http://192.168.1.2/register.php";
+    public static String URL_REGISTER = "http://mbakanova2.temp.swtest.ru/register.php";
 
-    public static String BASE_URL = "http://192.168.1.2/";
+    public static String BASE_URL = "http://mbakanova2.temp.swtest.ru/";
 
-    public static String BASE_PUBLIC = "http://192.168.1.2/public/";
+    public static String BASE_PUBLIC = "http://mbakanova2.temp.swtest.ru/public/";
 }
